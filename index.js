6a9582fccc339b3fe7fa9291d95753f5db99c74f
Prashant Lakal <lakalprashant66@gmail.com>
2023-02-10 16:51:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,182 @@
+// console.log("hello prashant")
+// var _a=20;
+// var $b=50;
+// console.log(_a+$b);
+// console.log(50-"30");  
+// console.log(50 + "30");
+// // console.log("prashant"+"lakal");
+// console.log("prashant"-"lakal");
+// console.log("   "+0);
+// var p=50;
+// var q=50;
+// console.log(p=q);  
+// // console.log( "is the p and q are equal" + p==q);
+// console.log(p==q);
+// console.log(`my numbers p and q are equal::::: ${p==q}`);
+// console.log(`mai ek software devoloper hoon ${p+q}`);
+// var mynumber=9922598291;
+// var p="prashant";
+// // console.log(isNaN(mynumber));
+// // console.log(isNaN(p));
+
+// var nav="my name is prashant";
+// var name=8208410582;
+// console.log(isNaN(name));
+// if(isNaN(name)){
+//  console.log("this is not a valid number");
+// }
+
+
+// var a=42;
+// var b=7;
+// var temp=a;
+// a=b;
+// b=temp;
+// console.log(swap(a,b));
+
+// console.log(~a);
+// console.log(~b);
+
+
+// console.log(a!=b);
+// console.log(a==b);
+// console.log(a+b)
+// console.log(a - b)
+// console.log(a / b)
+// console.log(a * b)
+// console.log("remainder operator",a % b)
+// console.log(a++);
+// console.log(++a);
+// console.log(a)
+// console.log(--a);
+// console.log( a--);
+// console.log(a)
+
+
+// console.log("a and b are equals or not",a==b);
+// console.log(`a and b are equals or not :${a == b}`);  
+
+// for (var num = 1; num <= 10; num++) {
+//  var tableof = 10;
+//  console.log(tableof + " * " + num + " = " + tableof * num);
+// }
+
+//  function addition(a,b){
+//   return a+b;
+// //  var addition=a+b;
+// //  console.log(addition);
+
+//  }
+//  addition(10,50);
+
+//  addition(50,40);
+// addition(40, 60);
+// addition(40, 20);
+// addition( );
+
+// function sum(a,b){
+//  return a+b;
+// }
+// sum(34,67);
+
+// var1 = 'prashant';
+// var2 = "89";
+// console.log(`my full name is ${var1}is the first name and last name is${var2} ok its my full name`)
+// let friends=["prashant","pravin","pavan","parth"];
+// for(let i=0;i<friends.length();i++){
+//  console.log(friends[i]);
+// }
+// console.log("hello pravin ");
+// let friends=["prashant","pravin","pavan","parth"];
+// for(let i=0;i<friends.length;i++){
+//  console.log(friends[i]);
+// }
+
+// let friends = ["prashant", "pravin", "pavan", "parth"];
+// for(let elements in friends){
+//  console.log( elements)
+// }
+// let family=["pravin","vishnu","savita","pooja","pavan","parth","prashant"];
+// family.forEach(function(element,index,array){
+//  console.log(element+"   index is  " +index+" "+array);
+// })
+
+
+// let family = ["pravin", "vishnu", "savita", "pooja", "pavan", "parth", "prashant"];
+// let friends=["pra","prav","pav","part","pooj"];
+//  let fam=(family.concat(friends));
+// console.log(family.concat(friends()))
+// family.forEach((element, index, array) => {
+//  console.log(element + "   index is  " + index + " " + array);
+// })
+// console.log(family.indexOf("parth",2));
+// console.log(family.indexOf("savita",3))
+// console.log(family.includes("pooja"));
+// console.log(family.includes("pooj"));
+ 
+// let family = ["pravin", "vishnu", "savita", "pooja", "pavan", "parth", "prashant"];
+// let friends = ["pra", "prav", "pav", "part", "pooj"];
+// let fam = family.concat(friends);
+// console.log(fam);
+// var _name,age ="prashant";
+// console.log(_name)
+// "prashant"-"pravin";
+// console.log("90   " +"89");
+// console.log(false-true)
+// var mynumber=8208410582/;
+// var myname="prashant";
+// console.log(isNaN(mynumber))
+// // console.log(isNaN(myname))
+// var x=20;
+// var y=20;
+// console.log("my name is "+ x === y);
+// console.log(`Is the two variables are equal::- ${x===y}`);
+// var a=30;
+// var b=30;
+// console.log(a!=b);
+// var age;
+// console.log("please enter age");
+// var prompt(age);
+// if(true){
+//  console.log("you can vote");
+// }
+// else{
+//  console.log("you cant vote");
+// }
+
+// ternary operator
+
+// var age=20;
+// console.log(( age > 18)? "you can vote" : "you cant vote");
+
+
+// var pi=3.142,l=4,b=5,r=3;
+// var area="square";
+// if(area=="circle"){
+//  console.log("area of circle is",pi*r*r);
+// }
+// else if(area=="rectange"){
+//  console.log("area of circle is", (l*b));
+// }
+// else if (area == "triangle"){
+//  console.log("area of circle is", (l * b)/2);
+// }
+// else{
+//  console.log("something gone wrong");
+// }
+// var table=10;
+// for(var num=1;num<=10;num++){
+//  console.log(tableof +"*"+ num +"=" + tableof*num);
+// }
+// console.log("prashant"+10)
+// console.log("prashant" - 10)
+
+
+
+
+
+let x = myFunction(4, 3);    
+
+function myFunction(a, b) {
+ return a * b;   
+}           
---
+++ tut1.js
@@ -0,0 +1,42 @@
+// /1=funexp(29, 50);
+// var sum2 = funexp(10, 50);
+// console.log(sum1>sum2)
+//  addition(50,40);
+// addition(40, 60);
+// addition(40, 20);
+// addition( );
+  
+
+
+
+// ECMAscript 2015 or ES6 updates
+
+// 1) let and const  
+// 2) template  literals or string literals
+// string literals or template literals is used in case of the string
+// let tableof=12;
+// for(let num=1;num<=10;num++){
+//  console.log(`the table of '"12"' is upto 10 is ${tableof} * ${num} = ${tableof*num}`)
+// }
+
+// const sum=(a,b )=>{
+//  // var a;
+//  // var b;
+//  console.log(` the sum of a and b is ${a+b} `);
+/  / }
+// sum(10,20);
+
+
+// let sum=()=> `this  the sum of a  and b is ${(a=5)+(b=7)}`;
+// console.log(sum())
+// var friend=["prashant",5,'pravin',30];
+// console.log(friend);
+
+// var friend = ['prashant',"prath" 'pravin'];
+// for(var elements of friend){
+//  console.log(elements);
+// }
+
+var1='prashant';
+var2="lakal";
+console.log(`my full name is ${var1},is the first name and last name is${var2} ok its my full name`)
